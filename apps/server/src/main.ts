b042acb318931ee82cf90ddd201a67da7dc63e2d
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-04-23 20:23:09 +0000

    update bootstrap function

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,21 +9,21 @@ bootstrap(schema);
 
 import { faker } from '@faker-js/faker';
 
-const fakeUser = () => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  cin: faker.random.number(),
-  city: faker.address.city(),
-  address: faker.address.streetAddress(),
-  phone: faker.phone.phoneNumber(),
-});
+// const fakeUser = () => ({
+//   name: faker.name.findName(),
+//   email: faker.internet.email(),
+//   cin: faker.random.number(),
+//   city: faker.address.city(),
+//   address: faker.address.streetAddress(),
+//   phone: faker.phone.phoneNumber(),
+// });
 
-// generate fake medication
-const fakeMedication = () => {
-  const medication = {
-    name: faker.name.findName(),
-    price: faker.commerce.price(),
-    refundable: faker.random.boolean(),
-  };
-  return medication;
-};
+// // generate fake medication
+// const fakeMedication = () => {
+//   const medication = {
+//     name: faker.name.findName(),
+//     price: faker.commerce.price(),
+//     refundable: faker.random.boolean(),
+//   };
+//   return medication;
+// };
